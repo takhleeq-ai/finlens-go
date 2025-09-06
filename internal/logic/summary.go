104d1bfb5e92dee4dadfb65678c7379d1c25e1b5
Syed Ahsan Subhan <Subhan.Ahsan@gmail.com>
2025-09-06 02:10:28 +0100

    updated summary to round healthscore to 2dp

--- internal/logic/summary.go
+++ internal/logic/summary.go
@@ -1,12 +1,21 @@
 package logic
 
-import "github.com/takhleeq-ai/finlens-go/models"
+import (
+	"math"
+
+	"github.com/takhleeq-ai/finlens-go/models"
+)
 
 type Summary struct {
 	Income      float64 `json:"total_income"`
 	Expenses    float64 `json:"total_expenses"`
 	Surplus     float64 `json:"surplus"`
 	HealthScore float64 `json:"health_score"`
+}
+
+func roundFloat(val float64, precision uint) float64 {
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
 
 func GenerateSummary(txs []models.Transaction) Summary {
@@ -21,7 +30,7 @@ func GenerateSummary(txs []models.Transaction) Summary {
 	surplus := income - expenses
 	score := 0.0
 	if income > 0 {
-		score = (surplus / income) * 100
+		score = roundFloat((surplus/income)*100, 2)
 	}
 	return Summary{
 		Income:      income,
